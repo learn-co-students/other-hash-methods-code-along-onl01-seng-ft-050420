74237e78ef0f7ae75b7f7ff447774f7123c9bdbd
Irene Scott <irenejinscott@gmail.com>
2020-05-12 02:57:16 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,5 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  groceries.values.flatten #flatten makes all the keys come together to just return an array list complete
 end
